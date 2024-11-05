403c30d6e5229d83cc37d2b6d6aca46ea90768b1
Jade Ryan Blancaflor <131427861+blancaflor480@users.noreply.github.com>
2024-11-05 22:20:52 +0800

    Complete Assignment 1 : Fibonacci Sequence

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AssignmentsController } from './assignments/assignments.controller';
+import { AssignmentController } from './assignments/assignments.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, AssignmentsController],
+  controllers: [AppController, AssignmentController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('assignments')
-export class AssignmentsController {}
+export class AssignmentController {
+  @Get('fibonacci/:n')
+  fibonacci(@Param('n') n: number) {
+    const sequence = [];
+    let a = 0, b = 1;
+    for (let i = 0; i < n; i++) {
+      sequence.push(a);
+      [a, b] = [b, a + b];
+    }
+    return { sequence };
+  }
+}
